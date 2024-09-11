3f28c290c14cf781fb05c44fb01dd0f918227c7d
unknown <denzelkinyua11@gmail.com>
2024-09-11 16:51:23 +0300

    Commit: Registered Models

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = models.ImageField(null=False, blank=False)
+
+    def __str__(self):
+        return self.user.username
+
+class Projects(models.Model):
+    name = models.CharField(max_length=300)
+    url = models.URLField(unique=True, null=False)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=350)
+    content = models.TextField(null=False)
+    likes = models.IntegerField(default=0)
+    image = models.ImageField(null=True, blank=True, default='image.jpg')
+
+    def __str__(self):
+        return self.name
+
