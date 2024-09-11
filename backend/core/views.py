85b7ac94da2d6685137b0ee80391502206500a2b
unknown <denzelkinyua11@gmail.com>
2024-09-11 17:05:58 +0300

    Commit: Added a test view

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+# Test View
+@api_view(["GET"])
+def hello_world(request):
+    message = 'Hello World'
+
+    return HttpResponse(message)
