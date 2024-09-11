cbe169de2002de0fde3e658ec6472a9c41853f95
unknown <denzelkinyua11@gmail.com>
2024-09-11 16:51:06 +0300

    Commit: Registered models

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from core.models import Profile, Post, Projects
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Projects)
+admin.site.register(Post)
