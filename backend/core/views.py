9ad128b3a301dcee05fb61109fcc6708f7c88eab
unknown <denzelkinyua11@gmail.com>
2024-09-12 13:14:50 +0300

    Commit: Added posts views

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+
+from core.serializers import PostSerializer, ProjectSerializer
+from core.models import Post, Projects
 
 # Create your views here.
 # Test View
@@ -11,3 +15,25 @@ def hello_world(request):
     message = 'Hello World'
 
     return HttpResponse(message)
+
+# Views for our posts
+@api_view(["GET"])
+def get_all_posts(request):
+    posts = Post.objects.all()
+
+    serializer = PostSerializer(posts, many=True)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(["GET"])
+def get_post(request, pk):
+    post = get_object_or_404(Post, id=pk)
+
+    if not post:
+        return Response({"detail": "Post not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = PostSerializer(post, many=False)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
