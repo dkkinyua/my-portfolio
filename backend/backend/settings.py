c55debaf5d42b35761a8a6789d0e347b02224001
unknown <denzelkinyua11@gmail.com>
2024-09-12 18:05:09 +0300

    Commit: Added email settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,4 +133,13 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# REST Framework settings
+load_dotenv()
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_ADDRESS') 
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD') 
+
