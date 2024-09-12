ae8420df3a7b85edfeaf70e1a43acebea708bfcd
unknown <denzelkinyua11@gmail.com>
2024-09-12 13:15:30 +0300

    Commit: Added urls for get_posts and get_all_posts

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from core.views import hello_world
+from core.views import hello_world, get_all_posts, get_post
 
 urlpatterns = [
     path("", hello_world, name='hello'),
+    path("posts/", get_all_posts, name='all-posts'),
+    path("posts/<int:pk>/", get_post, name="post"),
 ]
