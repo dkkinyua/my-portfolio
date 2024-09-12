a13cc001546e52f1d3dd0128c1da1651c367f599
unknown <denzelkinyua11@gmail.com>
2024-09-12 18:05:38 +0300

    Commit: Added URL paths for projects and sending email

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from core.views import hello_world, get_all_posts, get_post
+from core.views import hello_world, get_all_posts, get_post, get_project, get_all_projects, send_contact_email
 
 urlpatterns = [
     path("", hello_world, name='hello'),
     path("posts/", get_all_posts, name='all-posts'),
     path("posts/<int:pk>/", get_post, name="post"),
+    path('projects/', get_all_projects, name='all-projects'),
+    path('projects/<int:pk>/', get_project, name='project'),
+    path('contact-me/', send_contact_email, name='contact-me'),
 ]
