08db23d522d5341a4b82526b3dacef09b3ce2df5
unknown <denzelkinyua11@gmail.com>
2024-09-12 18:06:19 +0300

    Commit: Added views

--- backend/core/views.py
+++ backend/core/views.py
@@ -7,6 +7,7 @@ from rest_framework.decorators import api_view
 
 from core.serializers import PostSerializer, ProjectSerializer
 from core.models import Post, Projects
+from core.utils import send_email
 
 # Create your views here.
 # Test View
@@ -34,6 +35,42 @@ def get_post(request, pk):
     
     serializer = PostSerializer(post, many=False)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+# Projects
+@api_view(["GET"])
+def get_all_projects(request):
+    projects = Projects.objects.all()
+
+    serializer = ProjectSerializer(projects, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(["GET"])
+def get_project(request, pk):
+    project = get_object_or_404(Projects, id=pk)
+
+    if not project:
+        return Response({"detail": "Project not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = ProjectSerializer(project, many=False)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(["POST"])
+def send_contact_email(request):
+    data = request.data
+
+    try:
+        name = data["name"]
+        email = data["email"]
+        customer_message = data["customer_message"]
+
+        send_email(name, email, customer_message)
+
+        return Response({"detail": "Email sent successfully"}, status=status.HTTP_200_OK)
+
+    except Exception as e:
+        return Response({"detail": f"{str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 
