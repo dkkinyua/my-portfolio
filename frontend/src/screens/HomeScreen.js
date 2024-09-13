83e0eceb14963215727f0df97514b5268ab0cc1f
unknown <denzelkinyua11@gmail.com>
2024-09-13 17:33:06 +0300

    Commit: Added screens

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Intro from '../components/Intro'
+import { Container } from 'react-bootstrap'
+
+function HomeScreen() {
+  return (
+    <div className='justify-content-center text-center'>
+        <Container>
+            Intro
+        </Container>
+    </div>
+  )
+}
+
+export default HomeScreen
