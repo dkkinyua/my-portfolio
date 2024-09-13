2ee54a74eee55fbae6f5b8a3a2e06b2f28d75867
unknown <denzelkinyua11@gmail.com>
2024-09-13 17:00:34 +0300

    Commit: Added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { Container } from 'react-bootstrap'
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import TopHeader from './components/TopHeader';
+import Header from './components/Header';
 
 function App() {
   return (
     <Router>
+      <TopHeader />
+      <Header />
       <Container>
-        <TopHeader />
         <header className="App-header">
           Welcome to my Portfolio!
         </header>
