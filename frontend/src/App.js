f102ba23f2bb7b27877949f95c08591d1b79ab58
unknown <denzelkinyua11@gmail.com>
2024-09-13 17:32:35 +0300

    Commit: Added HomeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,22 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import TopHeader from './components/TopHeader';
 import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
     <Router>
       <TopHeader />
       <Header />
-      <Container>
-        <header className="App-header">
-          Welcome to my Portfolio!
-        </header>
-      </Container>
+      <main>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+          </Routes>
+        </Container>
+      </main>
+      <Footer/>
     </Router>
   );
 }
