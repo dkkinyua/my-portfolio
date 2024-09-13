9617c5133a3ef7033991dd53565321f7265427df
unknown <denzelkinyua11@gmail.com>
2024-09-13 12:29:26 +0300

    Commit: Added social media top header component

---
+++ frontend/src/components/TopHeader.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
+
+function TopHeader() {
+    const linkStyle = {
+        color: 'inherit',
+        textDecoration: 'none',
+    };
+
+    return (
+        <div className='d-flex justify-content-center'>
+            <Row>
+                <Col xs={3}>
+                    <a href='https://github.com/dkkinyua' style={linkStyle}><i className="fa-brands fa-github fa-2x"></i></a>
+                </Col>
+                <Col xs={3}>
+                    <a href='https://x.com/nyekssotrilll?t=t1b2X9PoKBlN11XM4Pt3YQ&s=09' style={linkStyle}><i className="fa-brands fa-x-twitter fa-2x"></i></a>
+                </Col>
+                <Col xs={3}>
+                    <a href='https://www.instagram.com/dkkinyua?igsh=dzFobWhraGpwNHh0' style={linkStyle}><i className="fa-brands fa-instagram fa-2x"></i></a>
+                </Col>
+                <Col xs={3}>
+                    <Link to='/contact-me' style={linkStyle}><i className='fa-regular fa-envelope open fa-2x'></i></Link>
+                </Col>
+            </Row>
+        </div>
+    );
+}
+
+export default TopHeader;
