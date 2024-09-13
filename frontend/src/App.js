b60cb2c15496e65f566957452ccd18eb35ab0b75
unknown <denzelkinyua11@gmail.com>
2024-09-13 12:28:27 +0300

    Commit: Registered our TopHeader component for social links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container } from 'react-bootstrap'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+import TopHeader from './components/TopHeader';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container>
+        <TopHeader />
+        <header className="App-header">
+          Welcome to my Portfolio!
+        </header>
+      </Container>
+    </Router>
   );
 }
 
