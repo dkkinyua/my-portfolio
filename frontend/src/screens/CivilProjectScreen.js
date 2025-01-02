78559873730286e48fc912b9e93a4e6bb0819645
unknown <denzelkinyua11@gmail.com>
2025-01-02 23:49:38 +0300

    Commit: Added a paragraph to civil-projects/

--- frontend/src/screens/CivilProjectScreen.js
+++ frontend/src/screens/CivilProjectScreen.js
@@ -26,6 +26,7 @@ function CivilProjectScreen() {
         <div style={textStyle} className='text-center'>
           <h2> PROJECTS </h2>
           <p className='mt-2'>Any calculations or testimonials will be provided upon request.</p>
+          <p className='mt-2'><strong>NOTE:</strong>So as to access these files on your laptop or phone, you need to download the relevant software to view them. For mobile, try DWG viewer to open .dwg files</p>
         </div>
         <div style={divStyle}>
           <CivilProjectCard />
